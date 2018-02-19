4057c9aa4863eb6f345028f96bfcfc5e1bf242a5
Kate Trahan <kathrynceciliatrahan@gmail.com>
2018-02-19 13:34:08 -0800

    erase extra comments

--- js/year.js
+++ js/year.js
@@ -3,66 +3,42 @@ export class Year {
     this.year = year;
 
   }
-
   calculateAge() {
     const secondsInMinute = 60;
     const minutesInHour =60;
     const hoursInADay =24;
     const daysinAYear =364.25;
 
-    let seconds = this.year * secondsInMinute * minutesInHour * hoursInADay * daysinAYear; // turns years into seconds
+    let seconds = this.year * secondsInMinute * minutesInHour * hoursInADay * daysinAYear;
       console.log(seconds);
       return seconds;
-
     }
-
   calculateMercuryAge() {
     let mercuryYears = Math.round(this.year / .24);
     console.log(mercuryYears);
     return mercuryYears;
-
-    // math is wrong :(
-    // const secondsInMinute = 60;
-    // const minutesInHour =60;
-    // const hoursInADay =24;
-    // const daysinAYear =364.25;
-    //
-    // let mercurySeconds = this.year * secondsInMinute * minutesInHour * hoursInADay * daysinAYear * .24;
-    // // return mercurySeconds;
-    // let mercuryYears = Math.round(mercurySeconds * 0.0000000316887);
-    // console.log(mercuryYears);
-    // return mercuryYears;
-
   }
-
   calculateVenusAge() {
     let venusYears = Math.round(this.year / .62);
     console.log(venusYears);
     return venusYears;
   }
-
   calculateMarsAge() {
     let marsYears = Math.round(this.year / 1.88);
     console.log(marsYears);
     return marsYears;
   }
-
   calculateJupiterAge() {
     let jupYears = Math.round(this.year / 11.86);
     console.log(jupYears);
     return jupYears;
   }
-
   calulateEarthLifeExpectancy() {
     let earthLifeExpectancy = 71 - this.year;
     return earthLifeExpectancy
   }
-
   calculateLifeExpectancyonMercury() {
     const averageLifeOnMercury = Math.round(this.year / .62);
     let mercuryLifeExpectancy = this.year - averageLifeOnMercury;
   }
-
-
-
 }
--- spec/years-spec.js
+++ spec/years-spec.js
@@ -4,47 +4,33 @@ describe('Year' , function() {
   it ('should return number of seconds in given amount of years', function() {
     let year = 24;
     let sample = new Year(year);
-    expect(sample.calculateAge()).toEqual(755308800); // not sure how to test without putting literal seconds in.
-    expect(sample.calculateAge()).toEqual(year * 60 * 60 * 24 * 364.25); // tests the same thing just broken up.
+    expect(sample.calculateAge()).toEqual(755308800);
+    expect(sample.calculateAge()).toEqual(year * 60 * 60 * 24 * 364.25);
   });
-
   it('should return number of Mercury Years', function() {
     let year = 24;
     let sample = new Year(year);
     expect(sample.calculateMercuryAge()).toEqual(Math.round(year / .24));
   });
-
   it('should return number of Venus Years', function() {
     let year = 24;
     let sample = new Year(year);
     expect(sample.calculateVenusAge()).toEqual(Math.round(year / .62));
     expect(sample.calculateVenusAge()).not.toEqual(24);
   });
-
   it('should return number of Mars Years', function() {
     let year = 24;
     let sample = new Year(year);
     expect(sample.calculateMarsAge()).toEqual(Math.round(year / 1.88));
   });
-
   it('should return number of Jupiter Years', function() {
     let year = 24;
     let sample = new Year(year);
     expect(sample.calculateJupiterAge()).toEqual(Math.round(year/ 11.86));
   });
-
   it('should return years left on earth', function() {
     let year = 24;
     let sample = new Year(year);
     expect(sample.calulateEarthLifeExpectancy()).toEqual(Math.round(71 - year))
   });
-
-  //This test somehow breaks browserify??????
-
-  // it('should return years left on mercury', function() {
-  // const averageLifeOnMercury = Math.round(this.year / .62);
-  //   let year = 24;
-  //   let sample = new Year(year);
-  //   expect(sample.calculateLifeExpectancyonMercury()).toEqual(Math.round(averageLifeOnMercury - (sample.calculateMercuryAge));
-  // });
 });
